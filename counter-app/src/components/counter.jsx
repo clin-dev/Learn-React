e9175d6363eee1895dd735f6670e4a2c1a8bbcdb
clin-dev <chaolinle@gmail.com>
2019-12-14 22:45:21 -0500

    event listener

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,22 +2,59 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0
+    //tags: []
   };
+
+  /*
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  */
 
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          //passing function reference!
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
+  }
+
+  // arrows function don't need constructor to bind
+  handleIncrement = product => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
+
+  /*
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }*/
 }
 
 export default Counter;
