6047889dae1d26890306a61478d1060ba2343437
clin-dev <chaolinle@gmail.com>
2019-12-23 22:57:30 -0500

    composing multiple conponents + children

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
     //tags: []
   };
 
@@ -14,8 +14,10 @@ class Counter extends Component {
   */
 
   render() {
+    //console.log(this.props);
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           //passing function reference!
@@ -30,18 +32,18 @@ class Counter extends Component {
 
   // arrows function don't need constructor to bind
   handleIncrement = product => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 
   /*
