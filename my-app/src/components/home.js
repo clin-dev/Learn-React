96427d1509ef123b131fbe2cc0129ad30a6e7b56
clin-dev <chaolinle@gmail.com>
2020-02-08 18:36:09 -0500

    clean up

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import "./css/home.css";
+import CypherText from "react-cypher-text-loop";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h1> Home Page!! </h1>
+        <CypherText
+          className="transitionText"
+          delay={2000}
+          textList={["Goal Oriented", "Optimistic", "Confident"]}
+        />
       </div>
     );
   }
